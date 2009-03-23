5b82e3c7e957153901ccbe992be913f85bfda3a8
Visnu Pitiyanuvath <visnupx@gmail.com>
2009-03-22 23:49:00 -0700

    use net/http 'cuz I guess I have to

--- public/tropo.rb
+++ public/tropo.rb
@@ -1,14 +1,24 @@
 require 'uri'
-require 'open-uri'
+require 'net/http'
 
-def command c
-  c = URI.encode c
-  open "http://frotz.jankyteeth.com/?save=#{$currentCall.callerID}&command=#{c}" { |f| f.read }
+class Frotz
+  def type c
+    c = URI.encode c
+    url = "http://frotz.jankyteeth.com/?save=#{$currentCall.callerID}&command=#{c}" 
+    Net::HTTP.get_response(URI.parse(url)).body
+  end
 end
 
 answer
 wait 1000
 
-say command('look')
+f = Frotz.new
+say f.type('look')
+
+while true  r = ask "what do you want to do?", :choices => 'look, open, close, open mailbox, read, east, west, north, south'
+  if r.name == 'choice'
+    say f.type(r.value)
+  end
+end
 
 hangup
